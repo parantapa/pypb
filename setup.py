4c41b05daaadfa43b0dd2581219736d18c29e657
Parantapa Bhattacharya <pb@parantapa.net>
2013-03-02 13:13:30 +0530

    Remove the LICENSE from the setup file

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     author_email     = 'pb@parantapa.net',
     packages         = ['pypb'],
     url              = 'github.com/parantapa/pypb',
-    license          = 'LICENSE.txt',
     description      = 'Python modules for PB',
     long_description = open('README.md').read(),
     install_requires = [
