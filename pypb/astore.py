954a3f9a3a273458c244fff6c9dcaa3cc65b34db
Parantapa Bhattacharya <pb@parantapa.net>
2015-02-13 11:16:13 +0530

    Add astore for atomic text storage

---
+++ pypb/astore.py
@@ -0,0 +1,42 @@
+# encoding: utf-8
+"""
+Read and write data to a text file atomically.
+
+http://stackoverflow.com/questions/2333872/atomic-writing-to-file-with-python
+"""
+
+import os
+import os.path
+
+TMP_SUFFIX = ".astore-tmp"
+
+def make_consistant(fname):
+    """
+    Make the data consistant.
+    """
+
+    if os.path.exists(fname + TMP_SUFFIX):
+        if os.path.exists(fname):
+            os.path.remove(fname)
+        os.rename(fname + TMP_SUFFIX, fname)
+
+def read(fname, mode="rb"):
+    """
+    Slurp contents of the file.
+    """
+
+    make_consistant(fname)
+    with open(fname, mode) as fobj:
+        return fobj.read()
+
+def write(fname, text, mode="wb"):
+    """
+    Write contents to the file.
+    """
+
+    with open(fname + TMP_SUFFIX, mode) as fobj:
+        fobj.write(text)
+        fobj.flush()
+        os.fsync(fobj.fileno())
+    make_consistant(fname)
+
